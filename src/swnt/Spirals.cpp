42f04603b62e2b1a64bc2ffafe5d4832bb41f7a3
roxlu <diederick@apollomedia.nl>
2014-01-18 13:36:52 +0100

    Update

--- src/swnt/Spirals.cpp
+++ src/swnt/Spirals.cpp
@@ -346,7 +346,7 @@ void Spirals::draw() {
   glDisable(GL_DEPTH_TEST);
   glEnable(GL_BLEND);
   glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-  //glBlendFunc(GL_ONE, GL_ONE);
+  glBlendFunc(GL_ONE, GL_ONE);
   draw(1.0f);
   glDisable(GL_BLEND);
 }
